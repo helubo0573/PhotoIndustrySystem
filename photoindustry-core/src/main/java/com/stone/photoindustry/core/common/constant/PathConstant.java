fbb64e36e8be21cf3b97b50d4708434f882de35f
Administrator <Administrator@PC-20181216HPGA>
2021-05-27 09:16:48 +0800

    init

---
+++ photoindustry-core/src/main/java/com/stone/photoindustry/core/common/constant/PathConstant.java
@@ -0,0 +1,12 @@
+package com.stone.photoindustry.core.common.constant;
+
+public class PathConstant {
+
+	/**登录后进入工作页面**/
+	public static final String FrameMainPage="forward:/WEB-INF/jsp/Frame1/Main.jsp";
+	/**工作台**/
+	public static final String FrameWorkbenchPage="forward:/WEB-INF/jsp/Frame/Workbench.jsp";								//首页
+	
+	/***错误请求后的跳转页面**/
+	public static final String ErrorPage="forward:/404.jsp";
+}
