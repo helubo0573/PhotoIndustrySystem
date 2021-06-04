b1a95462baedb35778e3bc9bbf751a4d92a14283
helubo0573 <40160019+helubo0573@users.noreply.github.com>
2021-06-04 21:00:46 +0800

    111

---
+++ photoindustry-core/src/main/java/com/stone/photoindustry/core/common/web/interceptor/PermissionsInterceptor.java
@@ -0,0 +1,42 @@
+package com.stone.photoindustry.core.common.web.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
+
+@Service
+public class PermissionsInterceptor extends HandlerInterceptorAdapter
+{
+
+	@Autowired
+	@Qualifier("sysMenuService")
+//	private SysMenuService sysMenuService;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
+	{
+		if (SecurityContextHolder.getContext().getAuthentication() == null){
+			return true;
+		}
+
+		UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (user == null || user.getUsername() == null){
+			response.setContentType("text/html;charset=utf-8");
+			response.getWriter().print("您未登录，请登录后继续此操作!");
+			return false;
+		}
+		String url = request.getServletPath();
+		if (url == null || url.length() <= 0){
+			return true;
+		}
+		return true;
+	}
+}
---
+++ photoindustry-core/src/main/java/com/stone/photoindustry/core/common/web/listener/WebConfigContextListener.java
@@ -0,0 +1,52 @@
+package com.stone.photoindustry.core.common.web.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+import org.apache.log4j.Logger;
+
+import com.stone.photoindustry.core.common.uti.CacheUtil;
+
+
+
+
+public class WebConfigContextListener implements ServletContextListener, HttpSessionAttributeListener
+{
+
+	private static Logger logger = Logger.getLogger(WebConfigContextListener.class);
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event)
+	{
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event)
+	{
+		logger.info("启动加载...");
+
+		// 系统参数
+		CacheUtil.initSysConfig();
+	}
+
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent event)
+	{
+
+	}
+
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent event)
+	{
+
+	}
+
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent event)
+	{
+
+	}
+}
